e4d15d749020848c8e91a28f3ce118f90c89ea44
Ebenezer Aladesuyi <ebenezeraladesuyi@gmail.com>
2023-08-30 19:00:35 +0100

    three

--- src/pages/HomePages/Hero.tsx
+++ src/pages/HomePages/Hero.tsx
@@ -26,20 +26,20 @@ const Hero = () => {
         <div className="w-[85%] h-full flex flex-wrap justify-between">
 
           <div className="flex flex-col justify-center items-center md:items-baseline h-[] w-full md:w-[40%] ">
-            <div className="text-[30px] text-gHover font-bold text-center animate-pulse md:text-[35px] lg:text-[70px]">EBENEZER <br/>ALADESUYI</div>
+            <div className="text-[30px] text-gHover font-bold text-center animate-pulse md:text-[33px] lg:text-[70px]">EBENEZER <br/>ALADESUYI</div>
             <div className="text-[] text-gColor text-center animate-pulse md:text-left text-[13px] lg:text-[22px]">Software Engineer (Full-Stack, MERN-Stack)</div>
 
-            <button className="bg-gColor text-white mt-[10px] lg:mt-[20px] lg:w-[150px] cursor-pointer ">Contact Me</button>
+            <button className="bg-gColor text-white mt-[10px] lg:mt-[20px] md:w-[110px] md:text-[13px] lg:w-[150px] cursor-pointer ">Contact Me</button>
           </div>
 
           <div className="flex flex-col justify-between items-center justify-self-end h-[100px] w-full relative md:flex-row md:justify-center md:w-[60%]  z-[20]" >
             {/* <div></div> */}
-            <img className="absolute top-0 w-[290px] md:w-[400px] md:mr-[100px] lg:w-[500px]" src={eben} alt="eben" />
+            <img className="absolute top-0 w-[320px] md:w-[400px] md:mr-[100px] lg:w-[550px]" src={eben} alt="eben" />
           </div>
 
         </div>
 
-        <div className="absolute w-[300px] h-[300px] rounded-full bg-gColor bottom-[-50px] right-[-60px] md:w-[450px] md:h-[450px] md:bottom-[-80px] md:right-[-120px] lg:w-[550px] lg:h-[550px] lg:bottom-[-100px] lg:right-[-140px]"></div>
+        <div className="absolute w-[300px] h-[300px] rounded-full bg-gColor bottom-[-60px] right-[-60px] md:w-[450px] md:h-[450px] md:bottom-[-80px] md:right-[-120px] lg:w-[550px] lg:h-[550px] lg:bottom-[-100px] lg:right-[-140px]"></div>
 
     </div>
   )
